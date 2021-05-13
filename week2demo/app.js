35f2be521736c5f8d66f7d1bc89ba13201e8b792
Harun Mbaabu Mwenda <mbaabuharun8@gmail.com>
2021-05-13 20:17:05 +0300

    added barner section

--- week2demo/app.js
+++ week2demo/app.js
@@ -1,5 +1,11 @@
-function show() {
-	let showmenu  = document.querySelector(".menu");
+let showmenu  = document.querySelector(".menu");
 
+function show() {
 	showmenu.style.display = "block";
+} 
+
+window.onclick = function(event) {
+  if (event.target == showmenu ) {
+    showmenu.style.display = "none";
+  }
 }
