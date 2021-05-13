3d3677e4e38b84048137864416887d350a6c6d65
timothyAgevi <timothyagevi@gmail.com>
2021-05-13 21:50:49 +0300

    revision

--- week2demo/app.js
+++ week2demo/app.js
@@ -1,5 +1,12 @@
-function show() {
-	let showmenu  = document.querySelector(".menu");
+let showmenu  = document.querySelector(".menu");
 
+function show() {
 	showmenu.style.display = "block";
+	showmenu.style.color="yellow";
+} 
+
+window.onclick = function(event) {
+  if (event.target == showmenu ) {
+    showmenu.style.display = "none";
+  }
 }
---
+++ week2demo/script.js
@@ -0,0 +1,27 @@
+// function-Subprogram called to perform a particular task.
+// Varable-identifier used to represent a single data item.
+// Parameter-Variable used to store values ,within a function.
+// Arguments-Actual values stored within a function.
+
+
+// 3 ways of declaring a function
+        // 1.traditional way
+                    // function calAge(cyear,yob){
+                    //     return cyear-yob;
+                    
+                    // }
+                    // console.log(calAge(2021,2000))
+
+        // 2.embedding it in a variable
+                    // const age=function calAge(cyear,yob){
+                    //       return cyear-yob;  
+                    // }
+                    // console.log(age(2021,2000))
+
+        //3. Using Arrow ; arrow function must be enclosed in variable
+            // const age  = (cyear, yob)  => {
+            //     return cyear - yob; 
+            // }
+            //     console.log(age(2021,2000))
+
+            
