13301fc5a3effdd49cbf8ba7e46bb9d40ff3cae3
Harun Mbaabu Mwenda <mbaabuharun8@gmail.com>
2021-05-18 13:30:03 +0300

    phone number validation file

--- JavaScript/example/search.js
+++ JavaScript/example/search.js
@@ -1,5 +1,5 @@
 let str = "Are you Ok? Yes, I'm OK";
 
-let result = str.match(/OK/gi);
+let result = str.match(/OK /gi);
 
 console.log(result);
--- JavaScript/regex-example/app.js
+++ JavaScript/regex-example/app.js
@@ -1,7 +1,7 @@
 function ValidateEmail(inputText)
 {
 	
-var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+var mailformat = / ^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-] + @[a-zA-Z0-9-] + (?:\.[a-zA-Z0-9-]+)*$ /;
 
 if(inputText.value.match(mailformat))
 {
@@ -11,7 +11,9 @@ return true;
 }
 else
 {
+
 alert("You have entered an invalid email address!");
+
 document.form1.text1.focus();
 return false;
 }
