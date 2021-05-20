bb2231c9963fa5ddc18a3505cf770069ec5762f9
timothyAgevi <timothyagevi@gmail.com>
2021-05-20 19:24:50 +0300

    updated

--- JavaScript/example/validate/app.js
+++ JavaScript/example/validate/app.js
@@ -1,6 +1,6 @@
 function validate(){
-  var phoneNumber = document.getElementById('phone-number').value;
-  var phoneRGEX = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
-  var phoneResult = phoneRGEX.test(phoneNumber);
+  var mailNumber = document.getElementById('mail-number').value;
+  var mailRGEX = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+  var mailResult = mailRGEX.test(mailNumber);
   return false;
 }
