43aa4967f344191a13e3b82d542534efa76c6729
timothyAgevi <timothyagevi@gmail.com>
2021-05-21 21:48:49 +0300

    update url on app.js

--- JavaScript/profiledemo/app.js
+++ JavaScript/profiledemo/app.js
@@ -1,7 +1,7 @@
 async function getUsers() {
     // let url = 'https://raw.githubusercontent.com/Lux-tech-Academy-Boot-Camp/Front-end-Software-Development-Course/main/JavaScript/profiledemo/users.json';
     // 
-let url = `http://localhost:5000/getUsers`
+let url = `https://raw.githubusercontent.com/timothyAgevi/Portfolio/gh-pages/us.json`
     try {
         let res = await fetch(url);
         return await res.json();
