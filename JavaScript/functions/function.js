3679d216da2e4bedc13cc8b3633685d1c6e938b0
timothyAgevi <timothyagevi@gmail.com>
2021-06-01 14:40:31 +0300

    updated

---
+++ JavaScript/functions/function.js
@@ -0,0 +1,10 @@
+function multiply(a,b){
+  return a*b
+}
+multiply(2,3)
+// // second way to declare function,is within a variable
+// let greetings= function greetUser(){
+//     console.log(`hi dear`)
+// }
+// // use of arrow function
+// let greetings=(  ()=> console.log)
